0bafd2749dee4df6fc230256f6632c509b265e97
gichukipaul <gichukipaul043@gmail.com>
2020-05-22 13:50:22 +0300

    Some updates

--- app/src/main/java/io/alienlabs/gichukipaul/belikebill/MainActivity.java
+++ app/src/main/java/io/alienlabs/gichukipaul/belikebill/MainActivity.java
@@ -1,18 +1,13 @@
 package io.alienlabs.gichukipaul.belikebill;
 
-import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import com.nostra13.universalimageloader.core.ImageLoader;
 import com.squareup.picasso.Picasso;
-
-import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,17 +24,55 @@ public class MainActivity extends AppCompatActivity {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                // task ts = new task();
+                // ts.execute();
                 ts();
                 //swipeRefreshLayout.setRefreshing(false);
             }
         });
-       // swipeRefreshLayout.setRefreshing(false);
+        swipeRefreshLayout.setRefreshing(false);
     }
-    public  void ts(){
+
+    public void ts() {
         imageView1.setBackgroundColor(Color.parseColor("#00ff00"));
         Picasso.get().load("https://belikebill.ga/billgen-API.php?default=1")
                 .into(imageView1);
         swipeRefreshLayout.setRefreshing(false);
     }
+
+
+    /*
+    public class task extends AsyncTask<Void, Void, Bitmap> {
+        ImageLoader imageLoader;
+        SweetAlertDialog pDialog;
+
+        @Override
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new SweetAlertDialog(MainActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+            pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+            pDialog.setTitleText("Loading");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... voids) {
+
+            imageLoader = ImageLoader.getInstance();
+            //returns a bitmap
+            return imageLoader.loadImageSync("https://belikebill.ga/billgen-API.php?default=1");
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            //pDialog.dismissWithAnimation();
+            imageView1.setImageBitmap(bitmap);
+        }
+    }
+    */
+
 
 }
